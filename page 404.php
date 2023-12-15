afe26baef349bf349eb9a4f5c4073e15527816b5
Ethan-Schmitt <ethan.schmitt@edu.univ-fcomte.fr>
2023-12-15 17:28:55 +0100

    Mise en place de la page 404

--- page 404.php
+++ page 404.php
@@ -1,3 +1,11 @@
+<?php
+switch($_GET['code'])
+{
+case '404':header('Location: https://www.243tech.com/erreur-404.php');
+break;
+default:header('Location: https://www.243tech.com/');
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
